2fe3b72640aa0592835ad1370db3dbc45dc4c22b
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-12-05 19:20:54 +0100

    add comments

--- BNO055.cpp
+++ BNO055.cpp
@@ -45,41 +45,83 @@ int8_t BNO055::temperature()
     return (int8_t)read_reg(BNO_TEMP);
 }
 
+/*
+Set the BNO operation mode
+@param mode Operation mode, see BNO_OPERATION_MODE
+*/
 void BNO055::set_operation_mode(BNO_OPERATION_MODE mode)
 {
     return write_reg(OPR_MODE, mode);
 }
 
+/*
+Set the BNO power mode
+@param mode Power mode, see BNO_POWER_MODE
+*/
 void BNO055::set_power_mode(BNO_POWER_MODE mode)
 {
     return write_reg(PWR_MODE, mode);
 }
 
+/*
+Get the BNO system status byte
+@return Current status byte
+*/
 uint8_t BNO055::get_system_status()
 {
     return read_reg(SYS_STATUS);
 }
 
+/*
+Specify the acceleration output unit
+
+Note: Changes only take effect when the sensor is in CONFIGMODE
+@param unit Acceleration unit, see BNO_ACC_UNIT
+*/
 void BNO055::set_acceleration_unit(BNO_ACC_UNIT unit)
 {
     return set_reg_bit(UNIT_SEL, 0, unit);
 }
 
+/*
+Specify the angular rate output unit
+
+Note: Changes only take effect when the sensor is in CONFIGMODE
+@param unit Angular rate unit, see BNO_ANG_RATE_UNIT
+*/
 void BNO055::set_angular_rate_unit(BNO_ANG_RATE_UNIT unit)
 {
     return set_reg_bit(UNIT_SEL, 1, unit);
 }
 
+/*
+Specify the angle output unit
+
+Note: Changes only take effect when the sensor is in CONFIGMODE
+@param unit Angle unit, see BNO_ANG_UNIT
+*/
 void BNO055::set_angle_unit(BNO_ANG_UNIT unit)
 {
     return set_reg_bit(UNIT_SEL, 2, unit);
 }
 
+/*
+Specify the temperature output unit
+
+Note: Changes only take effect when the sensor is in CONFIGMODE
+@param unit Temperature unit, see BNO_TEMP_UNIT
+*/
 void BNO055::set_temperature_unit(BNO_TEMP_UNIT unit)
 {
     return set_reg_bit(UNIT_SEL, 4, unit);
 }
 
+/*
+Specify from which sensor temperature readings are taken
+
+Note: Changes only take effect when the sensor is in CONFIGMODE
+@param source Temperature source, see BNO_TEMP_SOURCE
+*/
 void BNO055::set_temperature_source(BNO_TEMP_SOURCE source)
 {
     return set_reg_bit(TEMP_SOURCE, 0, source);
